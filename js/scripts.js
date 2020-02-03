3216e24e9c1a688a01f33befdbdd4926cef9691e
Kolby Kruger <kolbykruger@gmail.com>
2020-02-03 12:36:35 -0500

    converging on a final design

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 //Responsive iFrame
 $('iframe[src*="youtube"],iframe[src*="vimeo"]').wrap('<div class="responsiveIframe"/>');
 
+$('.testimonial-slider').slick({
+  autoplay: true,
+  autoplaySpeed: 5000,
+  dots: false,
+  infinite: true,
+  fade: false,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow:'<button class="prev" title="Previous Slide"><i class="fal fa-chevron-left"></i></button>',
+  nextArrow:'<button class="next" title="Next Slide"><i class="fal fa-chevron-right"></i></button>'
+});
+
 //Slideshow
 $('.slideshow .container').slick({
   autoplay: true,
